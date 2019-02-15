5e7d2ae7f482006fe634944f5ac58ab770087ee9
Kaustubh <kkaustubh@us.ibm.com>
2019-02-15 05:58:05 -0800

    Added drawer navigation. Header left added with "Open" button to open the drawer.

--- app/config/router.js
+++ app/config/router.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { Button } from 'react-native';
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+  createDrawerNavigator
+} from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons'
 
 import { capitalizeFirstLetter } from "../helpers/string";
@@ -12,9 +18,10 @@ export const ContactStackNavigator = createStackNavigator(
   {
     Contacts: {
       screen: Contacts,
-      navigationOptions: {
+      navigationOptions: ({ navigation }) => ({
         title: "Contacts",
-      }
+        headerLeft: <Button title="Open" onPress={() => navigation.openDrawer()}/>
+      })
     },
     Details: {
       screen: Details,
@@ -33,9 +40,10 @@ export const NewContactStack = createStackNavigator(
   {
     NewContact: {
       screen: NewContact,
-      navigationOptions: {
-        title: "New Contact"
-      }
+      navigationOptions: ({ navigation }) => ({
+        title: "New Contact",
+        headerLeft: <Button title="Open" onPress={() => navigation.openDrawer()}/>
+      })
     }
   }
 );
@@ -44,35 +52,73 @@ export const MeStack = createStackNavigator(
   {
     Me: {
       screen: Me,
-      navigationOptions: {
-        title: "Me"
-      }
+      navigationOptions: ({ navigation }) => ({
+        title: "me",
+        headerLeft: <Button title="Open" onPress={() => navigation.openDrawer()}/>
+      })
     }
   }
 );
 
-const TabNavigator = createBottomTabNavigator({
-  Contacts: {
-    screen: ContactStackNavigator,
-    navigationOptions: {
-      tabBarLabel: "Contacts",
-      tabBarIcon: ({ tintColor }) => <Icon name="ios-list" size={35} color={tintColor}/>
+const TabNavigator = createBottomTabNavigator(
+  {
+    Contacts: {
+      screen: ContactStackNavigator,
+      navigationOptions: {
+        tabBarLabel: "Contacts",
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-list" size={35} color={tintColor}/>
+      }
+    },
+    NewContact: {
+      screen: NewContactStack,
+      navigationOptions: {
+        tabBarLabel: "New Contact",
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-add" size={35} color={tintColor}/>
+      }
+    },
+    Me: {
+      screen: MeStack,
+      navigationOptions: {
+        tabBarLabel: "Me",
+        tabBarIcon: ({ tintColor }) => <Icon name="ios-contact" size={35} color={tintColor}/>
+      }
     }
   },
-  NewContact: {
-    screen: NewContactStack,
-    navigationOptions: {
-      tabBarLabel: "New Contact",
-      tabBarIcon: ({ tintColor }) => <Icon name="ios-add" size={35} color={tintColor}/>
-    }
-  },
-  Me: {
-    screen: MeStack,
-    navigationOptions: {
-      tabBarLabel: "Me",
-      tabBarIcon: ({ tintColor }) => <Icon name="ios-contact" size={35} color={tintColor}/>
-    }
-  },
-});
+  {
+    initialRouteName: "Contacts",
+    order: ["Contacts", "NewContact", "Me"]
+  }
+);
 
-export default createAppContainer(TabNavigator);
+const DrawerNavigator = createDrawerNavigator(
+  {
+    Contacts: {
+      screen: ContactStackNavigator,
+      navigationOptions: {
+        drawerLabel: "New Contact",
+        drawerIcon: ({ tintColor }) => <Icon name="ios-list" size={25} color={tintColor}/>
+      }
+
+    },
+    NewContact: {
+      screen: NewContactStack,
+      navigationOptions: {
+        drawerLabel: "New Contact",
+        drawerIcon: ({ tintColor }) => <Icon name="ios-add" size={25} color={tintColor}/>
+      }
+    },
+    Me: {
+      screen: MeStack,
+      navigationOptions: {
+        drawerLabel: "Me",
+        drawerIcon: ({ tintColor }) => <Icon name="ios-contact" size={25} color={tintColor}/>
+      }
+    }
+  },
+  {
+    initialRouteName: "Contacts",
+    order: ["Contacts", "NewContact", "Me"]
+  }
+);
+
+export default createAppContainer(DrawerNavigator);
