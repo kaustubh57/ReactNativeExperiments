e09b5c59aa5a46825dfb2cc9fcd81741ba921d05
Kaustubh <kkaustubh@us.ibm.com>
2019-02-15 15:04:27 -0800

    Added fields for New Contact form. Created CustomTextInput component to handle text input details.

--- app/screens/NewContact.js
+++ app/screens/NewContact.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View } from 'react-native';
+
+import colors from '../config/colors';
+import CustomTextInput from '../components/TextInput/CustomTextInput';
+import PrimaryButton from '../components/Buttons/PrimaryButton'
+
+const fields = [
+  { placeholder: "First Name", stateKey: "firstName" },
+  { placeholder: "Last Name", stateKey: "lastName" },
+  { placeholder: "Email", stateKey: "email", keyboardType: "email-address" },
+  { placeholder: "Mobile Phone", stateKey: "mobilePhone" },
+  { placeholder: "Home Phone", stateKey: "homePhone" },
+  { placeholder: "City", stateKey: "city" },
+  { placeholder: "Birthday", stateKey: "birthday" },
+  { placeholder: "Registered", stateKey: "registered" },
+  { placeholder: "Username", stateKey: "username" }
+];
 
 class NewContact extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  onInputChange = (text, stateKey) => {
+    const mod = {};
+    mod[stateKey] = text;
+    this.setState(mod);
+  };
+
+  handleSubmit = () => {
+    alert("Submit");
+  };
+
   render() {
     return (
-      <View
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F5FCFF'}}
-      >
-        <Text>New Contacts Screen</Text>
-      </View>
+      <ScrollView style={{ backgroundColor: colors.background }}>
+        {
+          fields.map((field) => (
+            <CustomTextInput
+              key={field.stateKey}
+              placeholder={field.placeholder}
+              onChangeText={text => this.onInputChange(text, field.stateKey)}
+              {...field}
+            />
+          ))
+        }
+        <View style={{ marginTop: 20 }}>
+          <PrimaryButton
+            label="Save"
+            onPress={() => this.handleSubmit()}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
