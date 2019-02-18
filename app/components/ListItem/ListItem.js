3f9b407ce91c8b0338e8d9d2ae3efef72387a8a2
Kaustubh <kkaustubh@us.ibm.com>
2019-02-18 07:28:53 -0800

    Added `Swipeable` gesture to  list items.

--- app/components/ListItem/ListItem.js
+++ app/components/ListItem/ListItem.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableHighlight, Image, Platform } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, Image, Platform } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { capitalizeFirstLetter } from "../../helpers/string";
 import colors from '../../config/colors';
 import styles, { CHEVRON_SIZE } from './styles';
 
-const ListItem = ({contact, onPress}) => {
+class ListItem extends Component {
 
-  const fullName = `${capitalizeFirstLetter(contact.name.first)} `+
-                   `${capitalizeFirstLetter(contact.name.last)}`;
-  const arrowForward = Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward';
+  render() {
+    const {contact, onPress} = {...this.props};
+    const fullName = `${capitalizeFirstLetter(contact.name.first)} ` +
+      `${capitalizeFirstLetter(contact.name.last)}`;
+    const arrowForward = Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward';
 
-  return (
-    <TouchableHighlight
-      onPress={onPress}
-      underlayColor={colors.rowUnderlay}
-    >
-      <View style={styles.row}>
-        <Image
-          source={{uri: contact.picture.thumbnail}}
-          style={styles.avatar}
-        />
-        <View style={styles.nameContainer}>
-          <Text style={styles.name}>{fullName}</Text>
-          <Text style={styles.email}>{contact.email}</Text>
-        </View>
-        <View style={styles.chevronContainer}>
-          <Icon
-            name={arrowForward}
-            size={CHEVRON_SIZE}
-            color={colors.subtleText}
-            style={styles.chevron}
-          />
-        </View>
-      </View>
-    </TouchableHighlight>
-  );
+    return (
+      <Swipeable>
+        <TouchableHighlight
+          onPress={onPress}
+          underlayColor={colors.rowUnderlay}
+        >
+          <View style={styles.row}>
+            <Image
+              source={{uri: contact.picture.thumbnail}}
+              style={styles.avatar}
+            />
+            <View style={styles.nameContainer}>
+              <Text style={styles.name}>{fullName}</Text>
+              <Text style={styles.email}>{contact.email}</Text>
+            </View>
+            <View style={styles.chevronContainer}>
+              <Icon
+                name={arrowForward}
+                size={CHEVRON_SIZE}
+                color={colors.subtleText}
+                style={styles.chevron}
+              />
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    );
+  }
 };
 
 export default ListItem;
